212c40fb6794b5fc282b7a5c00c1ccde04aa9e1b
Patryk <krasowski.p@outlook.com>
2023-10-17 22:44:22 +0200

    created draggable slider class

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,9 @@
 import data from '../../data.json'
 
+const trendingContainer = document.querySelector('.trending__box')
+
 const card = () => {
 	const trendingBox = document.querySelector('.trending__box')
-
 
 	const currentTrendings = data.filter(data => data.isTrending === true)
 
@@ -35,6 +36,52 @@ const card = () => {
 		trendingBox.insertAdjacentHTML('beforeend', markup)
 	}
 }
+
+const draggableSlider = dragContainer => {
+	this.draggableContainer = dragContainer
+}
+
+class DraggableSlider {
+	parentEl
+	constructor(sliderContainer) {
+		this.parentEl = sliderContainer
+		this.mouseEventHandler()
+	}
+	getHorizontialPosition(e) {
+		const clickedHorizontialPosition = e.pageX
+	}
+	mouseEventHandler() {
+		this.parentEl.addEventListener('mousedown', this.getHorizontialPosition)
+	}
+}
+
+const trenindingDragSlider = new DraggableSlider(trendingContainer)
+
+// let isDragStart = false,
+// 	prevPageX,
+// 	prevScrollLeft
+
+// const dragStart = e => {
+// 	console.log(e.pageX)
+// 	isDragStart = true
+// 	prevPageX = e.pageX
+// 	prevScrollLeft = trendingContainer.scrollLeft
+// }
+
+// const dragStop = () => {
+// 	isDragStart = false
+// }
+// const draggableSlider = e => {
+// 	if (!isDragStart) return
+// 	e.preventDefault()
+// 	let positionFidd = e.pageX - prevPageX
+// 	trendingContainer.scrollLeft = prevScrollLeft - positionFidd
+// }
+
+// trendingContainer.addEventListener('mousedown', dragStart)
+// trendingContainer.addEventListener('mouseup', dragStop)
+// trendingContainer.addEventListener('mouseover', dragStop)
+// trendingContainer.addEventListener('mousemove', draggableSlider)
 
 const init = () => {
 	card()
